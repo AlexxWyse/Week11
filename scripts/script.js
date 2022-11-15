9cefc3d1715cc08a858131e72bb76deff2f4b117
Alex <lathamwyse123@gmail.com>
2022-11-15 15:17:38 -0800

    created areaCircle funcrion

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,13 @@ const greet = (name, time = 'day') => {
 }
 
 greet(`Alex`, 'Night');
+
+
+let rad = prompt('Enter a radius: ')
+
+let circleArea = (radius) => {
+    return Math.PI * radius * radius;
+}
+
+let area = circleArea(rad);
+alert(`The area of this circle's radius is ${area.toFixed(2)}!`)
