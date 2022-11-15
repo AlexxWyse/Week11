a83bdb43cd50f6fe6815e08bd098147d7c0385b2
Alex <lathamwyse123@gmail.com>
2022-11-15 15:38:44 -0800

    edited text using javascript

--- scripts/script.js
+++ scripts/script.js
@@ -8,8 +8,14 @@ greet(`Alex`, 'Night');
 let rad = prompt('Enter a radius: ')
 
 let circleArea = (radius) => {
-    return Math.PI * radius * radius;
+    const radiusP = document.querySelector('#radius')
+    const resultP = document.querySelector('#result')
+    radiusP.textContent = radius;
+    let CalculatedArea = Math.PI * radius * radius;
+    resultP.textContent = CalculatedArea;
+    return CalculatedArea;
 }
 
 let area = circleArea(rad);
 alert(`The area of this circle's radius is ${area.toFixed(2)}!`)
+
